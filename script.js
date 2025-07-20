27d979419816faa5e245fa0d86c69f8c0c4cc99d
Kaybhee <abdulkabirbadru@gmail.com>
2025-07-20 23:29:57 +0100

    form submission handling

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
-const API_LINK = 'https://localhost:5000/signup'
+const API_LINK = 'https://localhost:5000/api/signup'
 
 
-document.getElementById('signup-form').addEventListener('sumit', async ( event )=> {
+document.getElementById('signup-form').addEventListener('submit', async ( event )=> {
     event.preventDefault();
-    
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const number = document.getElementById('number').value;
+    const response = fecth(API_LINK, {
+        method: POST,
+        headers: {'content-type': 'application/json'},
+        body: JSON.stringify({username: name, email: email, number: number})
+    });
+    const data = await response.json();
+    if (response.ok) {
+        document.getElementById('signup-message').innerText = 'Signup successful!';
+    }
+    else {
+        document.getElementById()
+    }
 }
 )
 
