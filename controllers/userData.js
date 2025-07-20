3fd3d3477310b7056690310c5fa3a86d99b78415
Kaybhee <abdulkabirbadru@gmail.com>
2025-07-21 00:59:34 +0100

    user routes and signup controller

--- controllers/userData.js
+++ controllers/userData.js
@@ -12,6 +12,10 @@ export const resendUserOtp = async(req, res) => {
 export const userSignUp =  async(req, res) => {
     try {
         const signForm = new appForm(req.body);
+        const existingUser = await appForm.findOne({email: req.body.email});
+        if (existingUser) {
+            return res.status(400).json({message: "User already exist with this email", data: null});
+        }
         await signForm.save();
         res.status(201).json({message: "User created successfully", data: signForm})
     } catch (err) {
--- script.js
+++ script.js
@@ -1,26 +1,30 @@
-const API_LINK = 'https://localhost:5000/api/signup'
+const API_LINK = 'http://localhost:5000/api/sign-up'
 
 
-document.getElementById('signup-form').addEventListener('submit', async ( event )=> {
+document.getElementById('sign-up-sign-in').addEventListener('submit', async ( event )=> {
     event.preventDefault();
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
-    const number = document.getElementById('number').value;
-    const response = fecth(API_LINK, {
-        method: POST,
-        headers: {'content-type': 'application/json'},
-        body: JSON.stringify({username: name, email: email, number: number})
-    });
-    const data = await response.json();
-    if (response.ok) {
-        document.getElementById('signup-message').innerText = 'Signup successful!';
+    const password = document.getElementById('pass').value;
+    // console.log(name);
+    // console.log(password);
+    try {
+        const response = await fetch(API_LINK, {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({name, email, password})
+        });
+        console.log(response)
+        const data = await response.json();
+        if (response.ok) {
+            document.getElementById('signup-message').innerText = 'Signup successful!';
+        }
+        else {
+            document.getElementById('signup-message').innerText = 'Signup failed!' || data.message;
+        }
     }
-    else {
-        document.getElementById()
+    catch (err) {
+        // console.error( 'Error during signup:', err);
+        document.getElementById('signup-message').innerText = 'An error occured during signup.';
     }
-}
-)
-
-const 
-
-console.log(name)
+})
