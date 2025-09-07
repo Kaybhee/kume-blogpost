00640b6db8579ea292f4b6617527535c2403714a
Kaybhee <abdulkabirbadru@gmail.com>
2025-09-07 23:15:24 +0100

    app initialization

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ import morgan from 'morgan';
 import { MONGODB_URL } from './config/db.js'; // Adjusted import to match the new structure
 import  connectDB from './config/db.js';
 import userDetails from './routes/signupDetails.js';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 dotenv.config();
 
@@ -16,6 +23,8 @@ app.use(morgan('dev'))
 
 const PORT = 5000;
 connectDB();
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use('/api', userDetails);
